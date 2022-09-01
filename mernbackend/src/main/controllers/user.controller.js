664110bb2123ed5930df16c11df4ab464ff9009d
PeterKampete <peterkampete01@gmail.com>
2022-09-01 21:17:25 +0100

    trying to fix local storage setting to undefined issue

--- mernbackend/src/main/controllers/user.controller.js
+++ mernbackend/src/main/controllers/user.controller.js
@@ -55,33 +55,19 @@ const list = async (req, res) => {
 };
 
 const update = async (req, res) => {
-  let form = new formidable.IncomingForm();
-  form.keepExtensions = true;
-  form.parse(req, async (err, fields, files) => {
-    if (err) {
-      return res.status(400).json({
-        error: "Photo could not be uploaded",
-      });
-    }
+  try {
     let user = req.profile;
-    user = extend(user, fields);
+    user = extend(user, req.body);
     user.updated = Date.now();
-    if (files.photo) {
-      user.photo.data = fs.readFileSync(files.photo.path);
-      user.photo.contentType = files.photo.type;
-    }
-    try {
-      console.log("saved detail", user);
-      await user.save();
-      user.hashed_password = undefined;
-      user.salt = undefined;
-      res.json(user);
-    } catch (err) {
-      return res.status(400).json({
-        error: errorHandler.getErrorMessage(err),
-      });
-    }
-  });
+    await user.save();
+    user.hashed_password = undefined;
+    user.salt = undefined;
+    res.json(user);
+  } catch (err) {
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err),
+    });
+  }
 };
 
 const remove = async (req, res) => {
